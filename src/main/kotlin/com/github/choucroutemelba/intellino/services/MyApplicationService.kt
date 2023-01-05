a2ec7cccbe692485139995db008836e6756127ed
GitHub Action <action@github.com>
2023-01-05 20:51:28 +0000

    Template cleanup

--- src/main/kotlin/com/github/choucroutemelba/intellino/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/choucroutemelba/intellino/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.choucroutemelba.intellino.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.choucroutemelba.intellino.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/choucroutemelba/intellino/services/MyProjectService.kt
+++ src/main/kotlin/com/github/choucroutemelba/intellino/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.choucroutemelba.intellino.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.choucroutemelba.intellino.MyBundle
 
 class MyProjectService(project: Project) {
 
